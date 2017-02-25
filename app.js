88f62a99d3a1d9ff7cb2c42017df6f043bb34914
atamyo <tamayo.angeline@gmail.com>
2017-02-25 17:14:24 -0500

    Add shape detection

--- app.js
+++ app.js
@@ -50,7 +50,55 @@ app.post('/upload', function(req, res, next) {
 									// Read image using OpenCV library
 									
 									cv.readImage(image.path, function(err, im) {
+										if (err) throw err;
 										console.log("omg opencv");
+										
+										var lowThresh = 0;
+										var highThresh = 100;
+										var nIters = 2;
+										var minArea = 2000;
+
+										var BLUE  = [0, 255, 0]; // B, G, R
+										var RED   = [0, 0, 255]; // B, G, R
+										var GREEN = [0, 255, 0]; // B, G, R
+										var WHITE = [255, 255, 255]; // B, G, R
+
+										var width = im.width()
+										var  height = im.height()
+										  if (width < 1 || height < 1) throw new Error('Image has no size');
+
+										  var out = new cv.Matrix(height, width);
+										  im.convertGrayscale();
+										  im_canny = im.copy();
+										  im_canny.canny(lowThresh, highThresh);
+										  im_canny.dilate(nIters);
+
+										  contours = im_canny.findContours();
+
+										  for (i = 0; i < contours.size(); i++) {
+
+										    if (contours.area(i) < minArea) continue;
+
+										    var arcLength = contours.arcLength(i, true);
+										    contours.approxPolyDP(i, 0.01 * arcLength, true);
+
+										    switch(contours.cornerCount(i)) {
+										      case 3:
+										        out.drawContour(contours, i, GREEN);
+										        break;
+										      case 4:
+										        out.drawContour(contours, i, RED);
+										        break;
+										      default:
+										        out.drawContour(contours, i, WHITE);
+										    }
+										}
+
+										out.save(path.join(__dirname + '/uploads/out' + ext));
+										console.log('yeee');
+
+										/*
+										// Detect faces
 										im.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
 											
 											for (var i=0;i<faces.length; i++){
@@ -59,6 +107,7 @@ app.post('/upload', function(req, res, next) {
 											}
 											im.save(path.join(__dirname + '/uploads/out' + ext));
 										});
+										*/
 									});
 								}
 							)
